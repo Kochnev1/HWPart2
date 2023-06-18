a66d29d884aa912bcd80186937257bc7936a697b
Roman Kochnev <romaslog@icloud.com>
2023-06-18 23:05:35 +0500

    Total result

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        int invAmount = 100; //Начальный счет
+        int repAmount = 2000; //Сумма пополнения
+        int totalCsore = (invAmount + repAmount); //Итоговый счет
+
+        int bonus;
+        if (repAmount >= 1000) {
+            bonus = repAmount / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Баланс счета:" + totalCsore);
+        System.out.println("Начислено бонусов:" + bonus);
+
 
     }
 }
